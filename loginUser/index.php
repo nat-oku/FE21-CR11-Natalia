42fffbf322d5eb2b13a2833f617f40736fbdc0aa
Natalia <natalia_bochenek@hotmail.com>
2021-04-30 11:01:13 +0200

    creating index.php page

---
+++ loginUser/index.php
@@ -0,0 +1,34 @@
+<!-- No login necessary, info about the ogranisation, display all animals on a single web page (index.php). -->
+
+<?php
+  require_once 'components/db_connect.php';
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- CSS & fonts -->
+  <?php require_once 'components/boot_fonts.php' ?>
+  <link rel="stylesheet" href="css/style.css">
+
+  <title>Vienna Pet Adoption || Home</title>
+
+</head>
+<body>
+  <header>
+    <!-- navbar -->
+    <?php require_once 'partials/navbar.php' ?>
+    <?php require_once 'partials/hero.php' ?>
+
+  </header>
+  
+</body>
+</html>
