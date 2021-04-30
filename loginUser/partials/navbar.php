c70609037215bdd7ade2c9394ab90d9ed04f861e
Natalia <natalia_bochenek@hotmail.com>
2021-04-30 09:54:04 +0200

    adding basic structure to navbar

---
+++ loginUser/partials/navbar.php
@@ -0,0 +1,28 @@
+<!-- navbar for not logged in users -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a href="#" class="navbar-brand d-flex flex-column">
+      <i class="fas fa-paw"></i>
+      <h4>VPA</h4>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Our seniors</a>
+        </li>
+        <li class="nav-item">
+          <a class="btn nav-link" href="#">Register</a>
+        </li>
+        <li class="nav-item">
+          <a class="btn nav-link" href="#">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
