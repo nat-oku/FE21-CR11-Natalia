9e32f275264d09a57835fb05efc5f53bad926457
Natalia <natalia_bochenek@hotmail.com>
2021-04-30 17:55:47 +0200

    adding functionality to display all pets on home

--- loginUser/home.php
+++ loginUser/home.php
@@ -1,8 +1,47 @@
-<!-- Display all animals on a single web page (home.php). -->
+<!-- Display all animals on a single web page (home.php). the admin & the user should be able to see them -->
 
 <?php
+  session_start();
+
   require_once 'components/db_connect.php';
 
+  #session check allowing only logged-in users to see the content
+  if(!isset($_SESSION['adm']) && !isset ($_SESSION['user']) ) {
+    header("Location: index.php");
+    exit;
+  }
+
+  $backBtn = '';
+  //if it is a user it will create a back button to user's profile.php
+  if (isset($_SESSION["user"])){
+   $backBtn = "profile.php";    
+  }
+  
+  //if it is a adm it will create a back button to dashboard.php
+  if(isset($_SESSION["adm"])){
+   $backBtn = "dashboard.php";    
+  }
+  $sql = "SELECT * FROM pets";
+  $colbody = '';
+  $result = mysqli_query($connect, $sql);
+  if(mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+      $colbody .='
+        <div class="col">
+          <div class="card">
+            <img src="pictures/pets/'.$row['picture'].'" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Meet '.$row['pet_name'].'</h5>
+              <p class="card-text">'.$row['pet_descr'].'</p>
+              <p class="card-text">Date of birth: '.$row['pet_date_of_birth'].'</p>
+              <p class="card-text">Type: a '.$row['pet_size'].' '.$row['breed']. '</p>
+              <p class="card-text">Address of shelter: '.$row['location']. '</p>
+            </div>
+          </div>
+        </div>
+      ';
+    }
+  }
 
 ?>
 
@@ -26,9 +65,17 @@
   <header>
     <!-- navbar -->
     <?php require_once 'partials/navbar.php' ?>
-    <?php require_once 'partials/hero.php' ?>
 
   </header>
+
+  <main>
+    <div class="container">
+      <div class="row row-cols-1 row-cols-md-2 g-4">
+        <?= $colbody ;?>
+      </div>
+    </div>
+
+  </main>
   
 </body>
 </html>
