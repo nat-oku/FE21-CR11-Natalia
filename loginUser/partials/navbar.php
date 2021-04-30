d5e4ad4185cd9f8ce326a407e8d4baeceb2c336a
Natalia <natalia_bochenek@hotmail.com>
2021-04-30 17:24:19 +0200

    adding profile.php link to navbar

--- loginUser/partials/navbar.php
+++ loginUser/partials/navbar.php
@@ -14,6 +14,9 @@
           <a class="nav-link" href="#">Home</a>
         </li>
         <li class="nav-item">
+          <a class="nav-link" href="profile.php">Your profile</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link" href="#">Our seniors</a>
         </li>
         <li class="nav-item">
