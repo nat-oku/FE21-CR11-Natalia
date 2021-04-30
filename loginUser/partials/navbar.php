63fb7139d8a1dd16b055ca0e0862381658f59058
Natalia <natalia_bochenek@hotmail.com>
2021-04-30 10:09:06 +0200

    adding register.php to navbar

--- loginUser/partials/navbar.php
+++ loginUser/partials/navbar.php
@@ -17,7 +17,7 @@
           <a class="nav-link" href="#">Our seniors</a>
         </li>
         <li class="nav-item">
-          <a class="btn nav-link" href="#">Register</a>
+          <a class="btn nav-link" href="register.php">Register</a>
         </li>
         <li class="nav-item">
           <a class="btn nav-link" href="#">Login</a>
