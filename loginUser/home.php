597318cf12172303c3677fb63b29ece71fd7b3c1
Natalia <natalia_bochenek@hotmail.com>
2021-05-01 12:04:09 +0200

    adding button and color styles to home

--- loginUser/home.php
+++ loginUser/home.php
@@ -27,7 +27,7 @@
   if(mysqli_num_rows($result) > 0) {
     while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
       $colbody .='
-        <div class="col">
+        <div class="col fw-light">
           <div class="card">
             <img src="pictures/pets/'.$row['picture'].'" class="card-img-top" alt="...">
             <div class="card-body">
@@ -38,7 +38,7 @@
               <p class="card-text">Address of shelter: '.$row['location']. '</p>
               <form action="home.php" method="post">
                 <input type="hidden" name="petID" class="form-control" value="'.$row['petID'].'"/>
-                <button class="btn" name="submitb" type="submit">Take me home</a></button>
+                <button class="btn border-gold" name="submitb" type="submit">Take me home</a></button>
               </form>
             </div>
           </div>
@@ -90,8 +90,14 @@
   </header>
 
   <main>
-    <div class="container">
-    <a href="<?php echo $backBtn?>" ><button class="btn btn-warning" type= "button">Back</button></a>
+    <div class="container mt-4 mb-4">
+      <div class="mb-3">
+        <h2>Welcome to Vienna Pet Adoption</h2>
+
+        <h4 class="fw-light"><i class="far fa-heart text-orange"></i> Here you can find all our beloved animals which we rescued in the last years. <i class="far fa-heart text-light"></i><br><i class="far fa-heart text-light"></i> They are very keen to meet you <i class="far fa-heart text-orange"></i><h4>
+        <h6 class="fw-light">With a click on the button "Take me home", you agree to adopt the selected pet.</h6>
+        <a href="<?php echo $backBtn?>" ><button class="btn bg-green text-light mt-1 mb-3" type="button">Back to your profile</button></a>
+      </div>
       <div class="row row-cols-1 row-cols-md-2 g-4">
         <?= $colbody ;?>
       </div>
