07aee76cdf6508369670d7a135c3ccca4111b575
Natalia <natalia_bochenek@hotmail.com>
2021-05-01 14:23:41 +0200

    commenting out db connection

--- loginUser/components/db_connect.php
+++ loginUser/components/db_connect.php
@@ -8,9 +8,10 @@
   // create connection
   $connect = new  mysqli($localhost, $username, $password, $dbname);
 
+  /*
   // check connection
   if($connect->connect_error) {
     die ("Connection failed: " . $connect->connect_error);
   } else {
       echo "Successfully Connected";
-  }
+  } */
